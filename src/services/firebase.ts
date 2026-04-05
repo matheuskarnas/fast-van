95c2675f3d594106667d8a8b534616ded7939493
matheuskarnas <matheuskarnas1@gmail.com>
2026-04-05 18:37:31 -0300

    fix(security): move firebase credentials to environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,19 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1nbmSj0VMhi_YNIp-I4l-xdK_rjM38Vc",
-  authDomain: "fastvan-31e7f.firebaseapp.com",
-  projectId: "fastvan-31e7f",
-  storageBucket: "fastvan-31e7f.firebasestorage.app",
-  messagingSenderId: "434556601934",
-  appId: "1:434556601934:web:1b30215dae07dcdec3651f"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-
